0e3ed7646c3029ee4731b22369e9c32729425ac5
odinakholiqov <bobojon.kholikov@gmail.com>
2024-04-02 15:52:31 +0500

    random movie

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,39 @@
+import random
+import json
 from flask import Flask
+from datetime import datetime
+
+# class to work with ORM
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+
+
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
 
 
 @app.route("/")
 def root():
-    return "Salom"
+    count = Opinion.query.count()
+
+    if not count:
+        return "No movies in DB"
+
+    offset_value = random.randrange(count)
+
+    opinion = Opinion.query.offset(offset_value).first()
+
+    return opinion.text
 
 
 if __name__ == "__main__":
