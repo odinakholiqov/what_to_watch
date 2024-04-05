80f127b2751dbf748201ad700e3ab6fbb8074613
odinakholiqov <bobojon.kholikov@gmail.com>
2024-04-05 10:41:03 +0500

    add opinion is done

--- opinions_app.py
+++ opinions_app.py
@@ -1,6 +1,6 @@
 import random
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -13,14 +13,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-app.config['SECRET_KEY'] = 'MY_SECRET_KEY'
+app.config["SECRET_KEY"] = "MY_SECRET_KEY"
 
 db = SQLAlchemy(app)
 
 
 class OpinionForm(FlaskForm):
-    title = StringField("Enter movie name", validators=[DataRequired(message="Required"), Length(1, 128)])
-    text = TextAreaField("Enter your opinion", validators=[DataRequired(message="Required")])
+    title = StringField(
+        "Enter movie name",
+        validators=[DataRequired(message="Required"), Length(1, 128)],
+    )
+    text = TextAreaField(
+        "Enter your opinion", validators=[DataRequired(message="Required")]
+    )
     source = URLField("Enter a link", validators=[Length(1, 256), Optional()])
     submit = SubmitField("Enter")
 
@@ -55,9 +60,18 @@ def opinion_view(id):
     return render_template("opinion.html", opinion=opinion)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add_opinion_view():
     form = OpinionForm()
+
+    if form.validate_on_submit():
+        opinion = Opinion(
+            title=form.title.data, text=form.text.data, source=form.source.data
+        )
+
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for("opinion_view", id=opinion.id))
 
     return render_template("add_opinion.html", form=form)
 
